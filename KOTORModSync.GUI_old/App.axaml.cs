a2f5eb5409cbb8f1f465d56718eb98871727cfc2
Benjamin Auquite <halomastar@gmail.com>
2023-05-13 19:16:28 -0500

    added notes

--- KOTORModSync.GUI_old/App.axaml.cs
+++ KOTORModSync.GUI_old/App.axaml.cs
@@ -3,7 +3,11 @@
 
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using KOTORModSync.GUI.ViewModels;
+using KOTORModSync.GUI.Views;
 
 namespace KOTORModSync.GUI
 {
@@ -18,7 +22,13 @@ namespace KOTORModSync.GUI
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow();
+                // Line below is needed to remove Avalonia data validation.
+                // Without this line you will get duplicate validations from both Avalonia and CT
+                ExpressionObserver.DataValidators.RemoveAll(x => x is DataAnnotationsValidationPlugin);
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
             }
 
             base.OnFrameworkInitializationCompleted();

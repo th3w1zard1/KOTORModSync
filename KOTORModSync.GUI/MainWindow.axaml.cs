25ec478baf3bf84513df2d0fff50b04d3a40bb6b
CSharp Lint <linter_workflow@githubactions.com>
2023-06-25 12:51:47 +0000

    Auto-format code using dotnet format

--- KOTORModSync.GUI/MainWindow.axaml.cs
+++ KOTORModSync.GUI/MainWindow.axaml.cs
@@ -258,7 +258,7 @@ namespace KOTORModSync
             return null;
         }
 
-        private async Task<bool> FindDuplicateComponents(List<Component> components )
+        private async Task<bool> FindDuplicateComponents( List<Component> components )
         {
             bool duplicatesFixed = true;
             // Check for duplicate GUID
@@ -586,7 +586,7 @@ namespace KOTORModSync
                         + " Please check the output window for more details.";
                 }
 
-                if ( informationMessage.Equals( string.Empty ))
+                if ( informationMessage.Equals( string.Empty ) )
                 {
                     informationMessage = "No issues found."
                         + " If you encounter any problems during the installation, please contact the developer.";
@@ -1391,7 +1391,7 @@ namespace KOTORModSync
                     throw new ArgumentNullException( nameof( component ) );
                 }
 
-                if ( !(parentItem.Items is AvaloniaList<object> parentItemItems) )
+                if ( !( parentItem.Items is AvaloniaList<object> parentItemItems ) )
                 {
                     throw new NullReferenceException( "parentItem must have a non-nullable Items property and be of type AvaloniaList<object>." );
                 }

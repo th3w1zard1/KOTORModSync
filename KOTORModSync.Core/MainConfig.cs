83ac058b3e2be1258030326097ad4b625d0f5ae0
Benjamin Auquite <halomastar@gmail.com>
2023-07-13 09:39:39 -0500

    Bump to v0.8.1

--- KOTORModSync.Core/MainConfig.cs
+++ KOTORModSync.Core/MainConfig.cs
@@ -21,7 +21,7 @@ namespace KOTORModSync.Core
     [SuppressMessage( "ReSharper", "InconsistentNaming" )]
     public class MainConfig : INotifyPropertyChanged
     {
-        public static string CurrentVersion => "0.8.0";
+        public static string CurrentVersion => "0.8.1";
 
         public MainConfig()
         {

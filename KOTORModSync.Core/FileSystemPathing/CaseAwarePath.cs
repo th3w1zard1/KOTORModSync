47d67baeeba33cf4a1f7ac33ce1c29167c3a9bf9
Benjamin Auquite <halomastar@gmail.com>
2023-09-07 23:14:04 -0500

    start integrating CaseAwarePath following pathlib.Path

---
+++ KOTORModSync.Core/FileSystemPathing/CaseAwarePath.cs
@@ -0,0 +1,213 @@
+﻿// Copyright 2021-2023 KOTORModSync
+// Licensed under the GNU General Public License v3.0 (GPLv3).
+// See LICENSE.txt file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace KOTORModSync.Core.FileSystemPathing
+{
+	[SuppressMessage(category: "ReSharper", checkId: "UnusedMember.Global")]
+	public class CaseAwarePath
+	{
+		private string _str;
+
+		public CaseAwarePath(params string[] args) :
+			this((object[])args) { } // Call the more general constructor directly
+
+		public CaseAwarePath(params object[] args)
+		{
+			_str = CombinePaths(args);
+			_str = FixPathFormatting(_str);
+
+			if ( ShouldResolveCase(_str) )
+			{
+				_str = GetCaseSensitivePath();
+			}
+
+
+			string[] parts = _str.Split(Path.DirectorySeparatorChar);
+			var result = new CaseAwarePath[parts.Length];
+			for ( int i = 0; i < parts.Length; i++ )
+			{
+				string cumulativePath = string.Join(Path.DirectorySeparatorChar.ToString(), parts.Take(i + 1));
+				result[i] = new CaseAwarePath(cumulativePath);
+			}
+
+			Name = Path.GetFileName(_str);
+			Parent = result.Length >= 2
+				? result[result.Length - 2]
+				: null;
+			Parts = result;
+			Root = Path.GetPathRoot(_str);
+			Stem = Path.GetFileNameWithoutExtension(_str);
+			Suffix = Path.GetExtension(_str);
+			//Drive
+			//Anchor
+			//Parents
+		}
+
+
+		public string Name { get; }
+		public CaseAwarePath Parent { get; set; }
+		public CaseAwarePath[] Parts { get; }
+		public string Root { get; set; }
+		public string Stem { get; set; }
+		public string Suffix { get; set; }
+
+		private static string ConvertObjectToPath(object arg)
+		{
+			switch ( arg )
+			{
+				case CaseAwarePath cap:
+					return cap.ToString();
+				case FileSystemInfo fsi:
+					return fsi.FullName;
+				case string s:
+					return s;
+				default:
+					throw new ArgumentException($"Unsupported type: {arg.GetType().FullName}");
+			}
+		}
+
+
+		private static string CombinePaths(params object[] args) =>
+			Path.Combine(args.Select(ConvertObjectToPath).ToArray());
+
+		public static CaseAwarePath operator /(CaseAwarePath p1, object p2) => p1.JoinPath(p2);
+
+		public static CaseAwarePath operator /(object p1, CaseAwarePath p2) => new CaseAwarePath(p1).JoinPath(p2);
+
+		public CaseAwarePath JoinPath(params object[] args)
+		{
+			var paths = args.Select(ConvertObjectToPath).ToList();
+
+			// If the base path (_str) is not rooted, simply combine all and return.
+			if ( !Path.IsPathRooted(_str) )
+			{
+				return new CaseAwarePath(
+					Path.Combine(_str, string.Join(Path.DirectorySeparatorChar.ToString(), paths))
+				);
+			}
+
+			string accumulatedPath = _str; // Start with the current instance path.
+
+			foreach ( string currentPath in paths )
+			{
+				// If path is rooted and contains the accumulated path hierarchy, intelligently combine.
+				if ( Path.IsPathRooted(currentPath)
+					&& currentPath.StartsWith(accumulatedPath, StringComparison.OrdinalIgnoreCase) )
+				{
+					accumulatedPath = currentPath; // Take the most detailed path.
+				}
+				else
+				{
+					accumulatedPath = Path.Combine(
+						accumulatedPath,
+						currentPath
+					); // Append both non-rooted paths and rooted ones that don't fit the accumulated hierarchy.
+				}
+			}
+
+			return new CaseAwarePath(accumulatedPath);
+		}
+
+
+		public CaseAwarePath Resolve()
+		{
+			_str = Path.GetFullPath(_str);
+			if ( ShouldResolveCase(_str) )
+			{
+				_str = GetCaseSensitivePath();
+			}
+
+			return this;
+		}
+
+		private string GetCaseSensitivePath()
+		{
+			var parts = _str.Split(Path.DirectorySeparatorChar).ToList();
+
+			for ( int i = 1; i < parts.Count; i++ )
+			{
+				string basePath = string.Join(Path.DirectorySeparatorChar.ToString(), parts.Take(i));
+				string nextPath = Path.Combine(basePath, parts[i]);
+
+				if ( !Directory.Exists(nextPath) && Directory.Exists(basePath) )
+				{
+					parts[i] = FindClosestMatch(
+						parts[i],
+						Directory.GetFileSystemEntries(basePath).Where(x => i == parts.Count - 1 || Directory.Exists(x))
+					);
+				}
+				else if ( !File.Exists(nextPath) && !Directory.Exists(nextPath) )
+				{
+					return Path.Combine(basePath, string.Join(Path.DirectorySeparatorChar.ToString(), parts.Skip(i)));
+				}
+			}
+
+			return string.Join(Path.DirectorySeparatorChar.ToString(), parts);
+		}
+
+		private static string FindClosestMatch(string target, IEnumerable<string> candidates)
+		{
+			int maxMatchingChars = -1;
+			string closestMatch = target;
+
+			foreach ( string candidate in candidates )
+			{
+				int matchingChars = GetMatchingCharactersCount(Path.GetFileName(candidate), target);
+				if ( matchingChars > maxMatchingChars )
+				{
+					maxMatchingChars = matchingChars;
+					closestMatch = candidate;
+					if ( maxMatchingChars == target.Length )
+						break;
+				}
+			}
+
+			return closestMatch;
+		}
+
+		private static int GetMatchingCharactersCount(string str1, string str2)
+		{
+			if ( string.Equals(str1, str2, StringComparison.OrdinalIgnoreCase) )
+				return -1;
+
+			return str1.Zip(
+				str2,
+				(c1, c2) => c1 == c2
+					? 1
+					: 0
+			).Sum();
+		}
+
+		private static string FixPathFormatting(string path)
+		{
+			if ( string.IsNullOrWhiteSpace(path) )
+				return path;
+
+			string formattedPath = path.Replace(oldValue: "\\", Path.DirectorySeparatorChar.ToString())
+				.Replace(oldValue: "/", Path.DirectorySeparatorChar.ToString());
+
+			formattedPath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+				? Regex.Replace(formattedPath, pattern: @"\\{2,}", replacement: @"\")
+				: Regex.Replace(formattedPath, pattern: @"/{2,}", replacement: "/");
+
+			return formattedPath.TrimEnd(Path.DirectorySeparatorChar);
+		}
+
+		private static bool ShouldResolveCase(string path) =>
+			!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+			&& Path.IsPathRooted(path)
+			&& !File.Exists(path)
+			&& !Directory.Exists(path);
+
+		public override string ToString() => _str;
+	}
+}
--- KOTORModSync.Core/FileSystemPathing/InsensitivePath.cs
+++ KOTORModSync.Core/FileSystemPathing/InsensitivePath.cs
@@ -9,51 +9,59 @@ using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using JetBrains.Annotations;
 
 namespace KOTORModSync.Core.FileSystemPathing
 {
 	// This class will provide easy usages of a case insensitive path.
-	// Calling Exists or FullName etc will always perform those checks case-insensitively.
-	[SuppressMessage(category: "ReSharper", checkId: "UnusedMember.Global")]
-	public sealed class InsensitivePath : DynamicObject
+	// By default (_realTimeUpdating = false), we only resolve case sensitivity on construction
+	// When _realTimeUpdating is true, calling Exists or FullName etc will always perform those checks case-insensitively.
+	[SuppressMessage( category: "ReSharper", checkId: "UnusedMember.Global" )]
+	public sealed class InsensitivePath
 	{
-		public InsensitivePath(FileSystemInfo fileSystemInfo)
+		private readonly bool _realTimeUpdating;
+		private readonly object _lockObject = new object();
+		[CanBeNull] private FileSystemInfo _fileSystemInfo { get; set; }
+		private readonly string OriginalPath;
+		public bool? IsFile { get; }
+
+		public InsensitivePath(FileSystemInfo fileSystemInfo, bool realTimeUpdating = false)
 		{
+			_realTimeUpdating = realTimeUpdating;
 			OriginalPath = fileSystemInfo.FullName;
-			_isFile = fileSystemInfo is FileInfo;
+			IsFile = fileSystemInfo is FileInfo;
 			_fileSystemInfo = fileSystemInfo;
-		}
-
-		public InsensitivePath(string inputPath, bool isFile)
-		{
-			string formattedPath = PathHelper.FixPathFormatting(inputPath);
-			_isFile = isFile;
-			_fileSystemInfo = _isFile
-				? new FileInfo(formattedPath)
-				: (FileSystemInfo)new DirectoryInfo(formattedPath);
-			OriginalPath = formattedPath;
-
+			
 			Refresh();
 		}
 
-		[NotNull] private FileSystemInfo _fileSystemInfo { get; set; }
-		private bool _isFile { get; }
-		public bool IsFile => _isFile;
-
-		public bool Exists
+		public InsensitivePath(string inputPath, bool? isFile = null, bool realTimeUpdating = false)
 		{
-			get
+			_realTimeUpdating = realTimeUpdating;
+			string formattedPath = PathHelper.FixPathFormatting(inputPath);
+			OriginalPath = formattedPath;
+			IsFile = isFile ?? DetermineIsFile(formattedPath);
+			if ( isFile == true )
+				_fileSystemInfo = (FileSystemInfo)new FileInfo(formattedPath);
+			else if ( isFile == false )
+				_fileSystemInfo = new DirectoryInfo(formattedPath);
+			Refresh();
+		}
+
+
+		public bool Exists => _fileSystemInfo?.Exists ?? ExistsRightNow();
+
+		private bool ExistsRightNow()
+		{
+			switch ( IsFile )
 			{
-				if ( IsFile && File.Exists(_fileSystemInfo.FullName) )
-					return true;
-				if ( !IsFile && Directory.Exists(_fileSystemInfo.FullName) )
-					return true;
-
-				if ( MainConfig.CaseInsensitivePathing )
-					Refresh();
-
-				return _fileSystemInfo.Exists;
+				case true:
+					return File.Exists(_fileSystemInfo.FullName);
+				case false:
+					return Directory.Exists(_fileSystemInfo.FullName);
+				default:
+					return File.Exists(_fileSystemInfo.FullName) || Directory.Exists(_fileSystemInfo.FullName);
 			}
 		}
 
@@ -61,11 +69,7 @@ namespace KOTORModSync.Core.FileSystemPathing
 		{
 			get
 			{
-				if ( Exists || !MainConfig.CaseInsensitivePathing )
-					return _fileSystemInfo.FullName;
-
-				if ( MainConfig.CaseInsensitivePathing )
-					Refresh();
+				_ = Exists;
 				return _fileSystemInfo.FullName;
 			}
 		}
@@ -74,92 +78,160 @@ namespace KOTORModSync.Core.FileSystemPathing
 		{
 			get
 			{
-				if ( Exists || !MainConfig.CaseInsensitivePathing )
-					return _fileSystemInfo.Name;
-
-				if ( MainConfig.CaseInsensitivePathing )
-					Refresh();
+				_ = Exists;
 				return _fileSystemInfo.Name;
 			}
 		}
 
-		private string OriginalPath { get; }
-
-		public List<FileSystemInfo> FindDuplicates() =>
-			PathHelper.FindCaseInsensitiveDuplicates(_fileSystemInfo.FullName, includeSubFolders: true, IsFile)
-				.ToList();
-
 		public void Delete()
 		{
+			_ = Exists;
 			_fileSystemInfo.Delete();
-			FindDuplicates()?.ToList().ForEach(duplicate => duplicate?.Delete());
+			GetDuplicates()?.ToList().ForEach(duplicate => duplicate?.Delete());
 		}
-
-		public override string ToString() => _fileSystemInfo.FullName;
 
 		public void Refresh()
 		{
 			_fileSystemInfo.Refresh();
 
-			if ( _fileSystemInfo.Exists )
-				return;
-			if ( !MainConfig.CaseInsensitivePathing )
+			// don't resolve case sensitivity if platform is windows or if the fileSystemInfo already exists.
+			if ( _fileSystemInfo.Exists || RuntimeInformation.IsOSPlatform(OSPlatform.Windows) )
 				return;
 
-			(string fileSystemItemPath, bool? isFile) = PathHelper.GetCaseSensitivePath(OriginalPath);
+			lock ( _lockObject )
+			{
+				if ( IsFile == null && ExistsRightNow() )
+					return;
 
-			switch ( isFile )
+				(string fileSystemItemPath, bool? isFile) = PathHelper.GetCaseSensitivePath(OriginalPath, IsFile);
+
+				switch ( isFile )
+				{
+					case true:
+						_fileSystemInfo = new FileInfo(fileSystemItemPath);
+						break;
+					case false:
+						_fileSystemInfo = new DirectoryInfo(fileSystemItemPath);
+						break;
+				}
+			}
+		}
+		
+		private static bool? DetermineIsFile(string path)
+		{
+			if (File.Exists(path))
+				return true;
+			if (Directory.Exists(path))
+				return false;
+			return null; // Neither file nor directory exists
+		}
+
+		public List<FileSystemInfo> GetDuplicates() =>
+			PathHelper.FindCaseInsensitiveDuplicates(_fileSystemInfo.FullName, includeSubFolders: false, IsFile)
+				.ToList();
+		
+		private Type GetTargetType()
+		{
+			switch ( IsFile )
 			{
 				case true:
-					_fileSystemInfo = new FileInfo(fileSystemItemPath);
-					break;
+					return typeof( FileInfo );
 				case false:
-					_fileSystemInfo = new DirectoryInfo(fileSystemItemPath);
-					break;
+					return typeof( DirectoryInfo );
 				default:
-					return;
+					return typeof( FileSystemInfo );
 			}
 		}
 
-		public override bool TryGetMember(GetMemberBinder binder, out object result)
-		{
-			MemberInfo memberInfo = _fileSystemInfo.GetType().GetMember(binder.Name).FirstOrDefault();
+	    public static InsensitivePath operator /(string a, InsensitivePath b)
+	    {
+	        string pathPart = PathHelper.FixPathFormatting(a);
+	        return CombineWithoutDuplicateCommonParts(pathPart, b.FullName);
+	    }
 
-			switch ( memberInfo )
-			{
-				case PropertyInfo propertyInfo:
-					result = propertyInfo.GetValue(_fileSystemInfo);
-					return true;
-				case MethodInfo methodInfo:
-					object Invoker(object[] args) => methodInfo.Invoke(_fileSystemInfo, args);
+	    public static InsensitivePath operator /(FileSystemInfo a, InsensitivePath b)
+	    {
+	        string pathPart = a.FullName;
+	        bool isFile = a is FileInfo;
+	        return CombineWithoutDuplicateCommonParts(pathPart, b.FullName, isFile);
+	    }
 
-					result = (Func<object[], object>)Invoker;
-					return true;
-				case FieldInfo fieldInfo:
-					result = fieldInfo.GetValue(_fileSystemInfo);
-					return true;
-				default:
-					result = null;
-					return false;
-			}
-		}
+	    public static InsensitivePath operator /(InsensitivePath a, string b)
+	    {
+	        string pathPart = PathHelper.FixPathFormatting(b);
+	        return CombineWithoutDuplicateCommonParts(a.FullName, pathPart);
+	    }
 
+	    public static InsensitivePath operator /(InsensitivePath a, FileSystemInfo b)
+	    {
+	        string pathPart = b.FullName;
+	        bool isFile = b is FileInfo;
+	        return CombineWithoutDuplicateCommonParts(a.FullName, pathPart, isFile);
+	    }
+
+	    private static InsensitivePath CombineWithoutDuplicateCommonParts(string pathA, string pathB, bool? isFile = null)
+	    {
+	        string[] componentsA = pathA.Split(Path.DirectorySeparatorChar);
+	        string[] componentsB = pathB.Split(Path.DirectorySeparatorChar);
+
+	        int overlapStartIndex = -1;
+
+	        for (int i = 0; i < Math.Min(componentsA.Length, componentsB.Length); i++)
+	        {
+	            if (componentsA[componentsA.Length - 1 - i] != componentsB[i])
+	                break;
+
+	            overlapStartIndex = i;
+	        }
+
+	        if (overlapStartIndex == -1)
+	            return new InsensitivePath(Path.Combine(pathA, pathB), isFile);
+
+	        string nonOverlappingA = string.Join(Path.DirectorySeparatorChar.ToString(), componentsA, startIndex: 0, componentsA.Length - overlapStartIndex);
+	        string nonOverlappingB = string.Join(Path.DirectorySeparatorChar.ToString(), componentsB, overlapStartIndex, componentsB.Length - overlapStartIndex);
+
+	        return new InsensitivePath(Path.Combine(nonOverlappingA, nonOverlappingB), isFile);
+	    }
+
+	    public InsensitivePath JoinPath(params object[] args)
+	    {
+	        InsensitivePath newPath = this;
+	        foreach (object arg in args)
+	        {
+		        switch ( arg )
+		        {
+			        case string strPath:
+				        newPath /= strPath;
+				        break;
+			        case FileSystemInfo fsInfo:
+				        newPath /= fsInfo;
+				        break;
+		        }
+	        }
+	        return newPath;
+	    }
+
+		public override string ToString() => FullName;
+		
+		public override int GetHashCode() => OriginalPath.ToLowerInvariant().GetHashCode();
+		public static bool operator ==(InsensitivePath left, object right) => left?.Equals(right) ?? right is null;
+		public static bool operator !=(InsensitivePath left, object right) => !(left == right);
 		public override bool Equals(object obj)
 		{
 			switch ( obj )
 			{
 				case string pathStr:
-					return string.Equals(_fileSystemInfo.FullName, pathStr, StringComparison.OrdinalIgnoreCase);
-				case FileSystemInfo fileInfo:
-					return string.Equals(
-						_fileSystemInfo.FullName,
-						fileInfo.FullName,
-						StringComparison.OrdinalIgnoreCase
-					);
+					return string.Equals(OriginalPath, pathStr, StringComparison.OrdinalIgnoreCase);
 				case InsensitivePath insensPath:
 					return string.Equals(
-						_fileSystemInfo.FullName,
+						OriginalPath,
 						insensPath._fileSystemInfo.FullName,
+						StringComparison.OrdinalIgnoreCase
+					);
+				case FileSystemInfo fileInfo:
+					return string.Equals(
+						OriginalPath,
+						fileInfo.FullName,
 						StringComparison.OrdinalIgnoreCase
 					);
 				default:
@@ -167,19 +239,10 @@ namespace KOTORModSync.Core.FileSystemPathing
 			}
 		}
 
-		// ReSharper disable once NonReadonlyMemberInGetHashCode - we only reference the string, not the object, so this warning can be ignored.
-		public override int GetHashCode() => _fileSystemInfo.FullName.ToLowerInvariant().GetHashCode();
-
-		public static bool operator ==(InsensitivePath left, object right) => left?.Equals(right) ?? right is null;
-		public static bool operator !=(InsensitivePath left, object right) => !(left == right);
-
-
 		public static implicit operator string(InsensitivePath insensitivePath) => insensitivePath.FullName;
+		public static implicit operator InsensitivePath(string pathString) => new InsensitivePath(pathString);
 
-		public static implicit operator FileInfo(InsensitivePath insensitivePath) =>
-			insensitivePath._fileSystemInfo as FileInfo;
-
-		public static implicit operator DirectoryInfo(InsensitivePath insensitivePath) =>
-			insensitivePath._fileSystemInfo as DirectoryInfo;
+		public static implicit operator FileInfo(InsensitivePath insensitivePath) => insensitivePath._fileSystemInfo as FileInfo;
+		public static implicit operator DirectoryInfo(InsensitivePath insensitivePath) => insensitivePath._fileSystemInfo as DirectoryInfo;
 	}
 }

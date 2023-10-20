a851dbb1e7fe8a823d703f9b3503104cf5cb28af
Benjamin Auquite <halomastar@gmail.com>
2023-10-20 13:32:44 -0500

    have publish script create mac's .app, ensure executable flags set

--- publish_release.ps1
+++ publish_release.ps1
@@ -3,6 +3,7 @@ $projectFile = "KOTORModSync.GUI\KOTORModSync.csproj"
 $publishProfilesDir = "KOTORModSync.GUI\Properties\PublishProfiles"
 $sevenZipPath = "C:\Program Files\7-Zip\7z.exe"  # Path to 7zip executable
 $publishProfileFiles = Get-ChildItem -Path $publishProfilesDir -Filter "*.pubxml"
+$bashLocation = "C:\Program Files\Git\bin\bash.exe"
 
 # Remove old builds if they exist.
 Get-ChildItem -Path "bin" -Filter "*.zip" | ForEach-Object {
@@ -45,7 +46,7 @@ foreach ($file in $publishProfileFiles) {
 
 
     # Build the dotnet publish command with the --framework argument
-    $publishCommand = "dotnet publish $projectFile --framework $framework /p:PublishProfile=$file"
+    $publishCommand = "dotnet publish $projectFile -c Release --framework $framework /p:PublishProfile=$file"
     Write-Host "Publish command: $publishCommand"
 
     try {
@@ -63,6 +64,66 @@ foreach ($file in $publishProfileFiles) {
 
         $publishFolder = Get-Item (Join-Path -Path (Split-Path -Path $projectFile) -ChildPath "..\bin\publish\$lastSection\$framework\$topLevelFolder")
 
+        # Set executable flag on our main EXE
+        $potentialExePaths = @(
+            (Join-Path -Path $publishFolder -ChildPath "KOTORModSync"),
+            (Join-Path -Path $publishFolder -ChildPath "KOTORModSync.exe")
+        )
+
+        # For EXE paths
+        foreach ($exePath in $potentialExePaths) {
+            if (Test-Path $exePath) {
+                Write-Host ""
+                Write-Host "Fixing EXE: $exePath"
+                $unixPath = $exePath -replace '\\', '/' -replace 'C:', '/c' -replace ' ', '\ '
+                $command = "& `"$bashLocation`" -c 'chmod +x $unixPath -v'"
+                Write-Host $command
+                Invoke-Expression $command
+            }
+        }
+
+        # Determine if macOS
+        $isMacOS = $rid -eq "osx-x64" -or $rid -eq "osx-arm64"
+        $destinationResourcesFolder = Join-Path -Path $publishFolder -ChildPath "Resources"
+
+        # macOS specific .app structure creation
+        if ($isMacOS) {
+            $appFolder = Join-Path -Path $publishFolder -ChildPath "KOTORModSync.app"
+            $contentsFolder = Join-Path -Path $appFolder -ChildPath "Contents"
+            $macOSFolder = Join-Path -Path $contentsFolder -ChildPath "MacOS"
+            $resourcesFolder = Join-Path -Path $contentsFolder -ChildPath "Resources"
+
+            # Create the directories
+            New-Item -Path $macOSFolder -ItemType Directory -Force
+
+            # Move all published files to the MacOS directory
+            Get-ChildItem -Path $publishFolder | Where-Object { $_.FullName -ne $appFolder } | ForEach-Object {
+                Move-Item -Path $_.FullName -Destination $macOSFolder
+            }
+
+            # Move the Resources folder from MacOS to the App root
+            $sourceResourcesFolder = Join-Path -Path $macOSFolder -ChildPath "Resources"
+            $destinationResourcesFolder = Join-Path -Path $appFolder -ChildPath "Resources"
+            Move-Item -Path $sourceResourcesFolder -Destination $destinationResourcesFolder
+
+            # Copy Info.plist to the Contents folder
+            Copy-Item -Path "./Info.plist" -Destination $contentsFolder
+        }
+
+        # Set executable permissions on our resources (holopatcher etc).
+        $resourceFiles = Get-ChildItem -Path $destinationResourcesFolder -Recurse -File
+        foreach ($file in $resourceFiles) {
+            if (Test-Path $file.FullName) {
+                Write-Host ""
+                $filePath = $file.FullName
+                Write-Host "Fixing resource: $filePath"
+                $unixPath = $filePath -replace '\\', '/' -replace 'C:', '/c' -replace ' ', '\ '
+                $command = "& `"$bashLocation`" -c 'chmod +x $unixPath -v'"
+                Write-Host $command
+                Invoke-Expression $command
+            }
+        }
+
         # Ensure 'docs' folder exists inside the $publishFolder
         $docsFolder = Join-Path -Path $publishFolder -ChildPath "docs"
         if (-not (Test-Path $docsFolder)) {
@@ -73,19 +134,25 @@ foreach ($file in $publishProfileFiles) {
         Copy-Item -Path "LICENSE.TXT" -Destination $docsFolder
         Copy-Item -Path "KOTORModSync - Official Documentation.txt" -Destination $docsFolder
 
+        if ($isMacOS) {
+            # Set the directory for the zip operation to the .app folder
+            $archiveSource = $appFolder
+        } else {
+            $archiveSource = $publishFolder
+        }
+
+        # Before archiving, set *.pdb files to hidden
+        Get-ChildItem -Path $archiveSource -Recurse -Filter "*.pdb" | ForEach-Object {
+            Set-HiddenAttribute -path $_.FullName
+        }
 
         # Define the archive file path
         $archiveFile = Join-Path -Path (Split-Path -Path "bin\publish") -ChildPath "$rid.zip"
 
-        # Before archiving, set *.pdb files to hidden
-        Get-ChildItem -Path $publishFolder -Recurse -Filter "*.pdb" | ForEach-Object {
-            Set-HiddenAttribute -path $_.FullName
-        }
-
         # Create the archive using 7zip CLI
-        $archiveCommand = "& `"$sevenZipPath`" a -tzip `"$archiveFile`" `"$publishFolder*`""
+        $archiveCommand = "& `"$sevenZipPath`" a -tzip `"$archiveFile`" `"$archiveSource*`""
         Invoke-Expression $archiveCommand
-        
+
         # Before deleting, set *.pdb files back to normal
         Get-ChildItem -Path "$publishFolder\..\$topLevelFolder" -Recurse -Filter "*.pdb" | ForEach-Object {
             Remove-HiddenAttribute -path $_.FullName
